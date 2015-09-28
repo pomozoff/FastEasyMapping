2b22612a528392a95ca327b20559d436d657de6c
Anton Pomozov <pomozov@shtrih-m.ru>
2015-09-28 17:02:38 +0300

    [Add]: Framework target for Carthage

---
+++ FastEasyMappingF/FastEasyMappingF.h
@@ -0,0 +1,19 @@
+//
+//  FastEasyMappingF.h
+//  FastEasyMappingF
+//
+//  Created by Anton Pomozov on 28.09.15.
+//  Copyright © 2015 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastEasyMappingF.
+FOUNDATION_EXPORT double FastEasyMappingFVersionNumber;
+
+//! Project version string for FastEasyMappingF.
+FOUNDATION_EXPORT const unsigned char FastEasyMappingFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastEasyMappingF/PublicHeader.h>
+
+#import "FastEasyMapping.h"
